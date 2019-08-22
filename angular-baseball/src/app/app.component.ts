05fe4ecd3e3d2a0c4b2dc5d628a1c62f307ecac3
Dan Russell <dan.russell@uconn.edu>
2019-08-22 12:43:27 -0400

    Attempts at hit variation, refactoring, linting

--- angular-baseball/src/app/app.component.ts
+++ angular-baseball/src/app/app.component.ts
@@ -1,46 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit/*, DoCheck,*/ } from '@angular/core';
+import { AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+// Implementing DoCheck and OnChanges in a class is not recommended
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements
+  OnChanges, OnInit, /*DoCheck,*/ AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  // Implementing DoCheck and OnChanges in a class is not recommended
   title = 'Angular Baseball';
 
-  pitchResult = 'Here comes the pitch...<br />';
+  pitchResult = '';
 
   pitchOutcome = 0;
 
   pa = {
-    "balls": 0,
-    "strikes": 0,
-    "inplay": false,
-    "hit": false,
-    "out": false
+    balls: 0,
+    strikes: 0,
+    inplay: false,
+    hit: false,
+    out: false
   };
 
   game = {
-    "pitches": 0,
-    "swings": 0,
-    "misses": 0,
-    "taken": 0,
-    "balls": 0,
-    "strikes": 0,
-    "BB": 0,
-    "K": 0,
-    "inplay": 0,
-    "hits": 0,
-    "outs": 0,
-    "inning": 1,
-    "innings": [],
-    "teams": {
-      "away": {
+    pitches: 0,
+    swings: 0,
+    misses: 0,
+    taken: 0,
+    balls: 0,
+    strikes: 0,
+    BB: 0,
+    K: 0,
+    inplay: 0,
+    hits: 0,
+    outs: 0,
+    inning: 1,
+    innings: [],
+    teams: {
+      away: {
         runs: 0,
         hits: 0,
         errors: 0
       },
-      "home": {
+      home: {
         runs: 0,
         hits: 0,
         errors: 0
@@ -48,138 +52,215 @@ export class AppComponent {
     }
   };
 
-  toporbot = "top";
+  messages = {
+    pitch: {
+      0: 'Here comes the pitch...',
+      1: 'The pitcher kicks and deals...'
+    },
+    hit: {
+      0: '<strong>Lands for a base hit.</strong>'
+    }
+  };
 
-  //game.innings[game.inning].top.runs = 0;
+  toporbot = 'top';
 
-  pitchOutput = "";
+  // game.innings[game.inning].top.runs = 0;
 
-  rand(min, max) {
+  MLB2018hits = 41019;
+  MLB20182B = 8264;
+  MLB20183B = 847;
+  MLB2018HR = 5585;
+  MLB20181B = this.MLB2018hits - this.MLB20182B - this.MLB20183B - this.MLB2018HR;
+
+  hitpercent1B = (this.MLB20181B / this.MLB2018hits) * 100;
+  hitpercent2B = (this.MLB20182B / this.MLB2018hits) * 100;
+  hitpercent3B = (this.MLB20183B / this.MLB2018hits) * 100;
+  hitpercentHR = (this.MLB2018HR / this.MLB2018hits) * 100;
+
+  pitchOutput = '';
+
+  rand(min: number, max: number) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  wherearewe(){
-    //alert(this.toporbot + ' ' + this.game.inning);
+  randObj(obj) {
+    const keys = Object.keys(obj);
+    return obj[keys[Math.floor(Math.random() * keys.length)]];
   }
 
-  switchsides(){
-    if(this.toporbot == "top"){
-      this.toporbot = "bot";
-    }
-    else{
-      this.toporbot = "top";
+  message(messagekind: string) {
+    let message = this.randObj(this.messages[messagekind]);
+    message += '<br /><br />';
+    return message;
+  }
+
+  wherearewe() {
+    // alert(this.toporbot + ' ' + this.game.inning);
+  }
+
+  switchsides() {
+    if (this.toporbot === 'top') {
+      this.toporbot = 'bot';
+    } else {
+      this.toporbot = 'top';
     }
   }
 
-  recordout(){
+  recordstrike(strikekind: string) {
+    this.game.strikes++;
+
+    this.pa.strikes++;
+
+    if (strikekind === 'looking') {
+      this.game.taken++;
+    }
+    if (strikekind === 'swinging') {
+      this.game.misses++;
+    }
+
+    if (this.pa.strikes === 3) {
+      this.resetpa();
+      this.pitchResult += '<strong>Steee-rike three! Struck out ' + strikekind + '.</strong><br/><br/>';
+      this.game.K++;
+      this.recordout();
+    }
+  }
+
+  recordball() {
+    this.game.balls++;
+
+    this.pa.balls++;
+
+    if (this.pa.balls === 4) {
+      this.resetpa();
+      this.pitchResult += '<strong>Ball four! That\'s a walk.</strong><br/><br/>';
+      this.game.BB++;
+    }
+  }
+
+  recordout() {
     this.game.outs++;
-    if((this.game.outs % 3) == 0){
-      //alert("End of half inning!");
+    if ((this.game.outs === (3 * 2 * 9))) {
+      this.pitchResult += '<br /><br /><strong>And that\'s the game!</strong>';
+    } else {
+      if ((this.game.outs % 3) === 0) {
+        // alert("End of half inning!");
 
-      this.game.innings[this.game.inning][this.toporbot].runs = 0;
+        this.game.innings[this.game.inning][this.toporbot].runs = 0;
 
-      if((this.game.outs % 6) == 0){
-        //alert("End of inning!");
-        this.game.inning++;
-        this.startinning();
+        if ((this.game.outs % 6) === 0) {
+          // alert("End of inning!");
+          this.game.inning++;
+          this.startinning();
+        }
+
+        this.switchsides();
+
+        this.wherearewe();
       }
+    }
 
-      this.switchsides();
+  }
 
-      this.wherearewe();
+  teambatting() {
+    if (this.toporbot === 'top') {
+      return 'away';
+    } else {
+      return 'home';
     }
   }
 
-  teambatting(){
-    if(this.toporbot == "top"){
-      return "away";
-    }
-    else{
-      return "home";
-    }
-  }
-
-  recordhit(){
+  recordhit() {
     this.game.hits++;
 
     this.game.teams[this.teambatting()].hits++;
+
+    this.pitchResult += this.message('hit');
+
+    const roll = this.rand(0, 100);
+
+    if (roll <= this.hitpercentHR) {
+      this.pitchResult += 'Going back... <strong>It\'s a Home Run!!!</strong><br /><br />';
+    }
+    if (roll <= this.hitpercent3B) {
+      this.pitchResult += 'What speed! <strong>It\'s a triple!</strong><br /><br />';
+    }
+    if (roll <= this.hitpercent2B) {
+        this.pitchResult += 'Gonna try for two.. <strong>In with a double.</strong><br /><br />';
+    }
+    if (roll <= this.hitpercent1B) {
+      this.pitchResult += 'A clean hit. <strong>On first with a single.</strong><br /><br />';
+    }
+
   }
 
   inplay() {
     this.pa.inplay = true;
+    this.game.inplay++;
 
-    if(this.rand(1, 100) <= 30) { //League-average BABIP = .300
+    if (this.rand(1, 100) <= 30) { // League-average BABIP = .300
       this.pa.hit = true;
 
       this.recordhit();
-      this.pitchResult += '<strong>Lands for a base hit!</strong><br/>';
-    }
-    else{
+    } else {
       this.pa.out = true;
 
-      this.recordout();
+      this.pitchResult += '<strong>Handled by the defense for an out.</strong><br/><br/>';
 
-      this.pitchResult += '<strong>Handled by the defense for an out.</strong><br/>';
+      this.recordout();
     }
 
+    this.resetpa();
   }
 
   swing(zone) {
-    var contact
-    var messageHit;
-    var messageMiss;
+    let contact: number;
+    let messageHit: string;
+    let messageMiss: string;
 
-    if(zone == 0){
+    if (zone === false) {
       contact = 66; // Contact? [O-Contact% - average 66%]
-      messageHit = "<strong>Slapped in play!</strong><br/>";
-      messageMiss = "Shoulda let that one go!<br/>";
+      messageHit = '<strong>Slapped in play!</strong><br/><br/>';
+      messageMiss = 'Shoulda let that one go!<br/><br/>';
     }
-    if(zone == 1){
+    if (zone === true) {
       contact = 87; // Contact? [Z-Contact% - average 87%]
-      messageHit = "<strong>It\'s in play!</strong><br/>";
-      messageMiss = "Got it by him!<br/>";
+      messageHit = '<strong>It\'s in play!</strong><br/><br/>';
+      messageMiss = 'Got it by him!<br/><br/>';
     }
 
     this.pitchResult += 'Swung on...<br/>';
     this.game.swings++;
 
     this.pitchOutcome = this.rand(1, 100);
-    if(this.pitchOutcome <= contact){
+    if (this.pitchOutcome <= contact) {
       // Contact!
       this.pitchResult += messageHit;
 
       this.inplay();
-    }
-    else{
+    } else {
       // Missed
       this.pitchResult += 'Swung on and missed for a strike.<br/>';
       this.pitchResult += messageMiss;
 
-      this.game.misses++;
-      this.game.strikes++;
-
-      this.pa.strikes++;
+      this.recordstrike('swinging');
     }
   }
 
-  take(zone){
-    var message;
+  take(zone: boolean) {
+    let message: string;
 
-    this.game.taken++;
+    if (zone === false) {
+      message = 'Taken for a ball. Good eye!<br/>';
 
-    if(zone == 0){
-      message = 'Taken for a ball.<br/>Good eye!<br/>';
-      this.game.balls++;
-
-      this.pa["balls"]++;
+      this.recordball();
     }
-    if(zone == 1){
+    if (zone === true) {
       message = 'Taken for a strike.<br/>';
-      this.game.strikes++;
 
-      this.pa["strikes"]++;
+      this.recordstrike('looking');
     }
 
     this.pitchResult += message;
@@ -206,60 +287,64 @@ export class AppComponent {
     this.game.inning = 1;
     this.game.inning = 1;
 
-    this.pitchResult = "";
-    this.pitchOutput = "";
+    this.pitchResult = '';
+    this.pitchOutput = '';
   }
 
-  startinning(){
+  startinning() {
     this.game.innings[this.game.inning] = {};
     this.game.innings[this.game.inning].top = {};
     this.game.innings[this.game.inning].bot = {};
   }
 
-  startgame(){
+  startgame() {
     //
     this.wherearewe();
 
+    /*
+    alert(this.hitpercent1B);
+    alert(this.hitpercent2B);
+    alert(this.hitpercent3B);
+    alert(this.hitpercentHR);
+    */
     this.startinning();
   }
 
   pitch() {
-    if (this.game.outs < (3*2*9)){
-      if (this.game.pitches == 0){
+    if (this.game.outs < (3 * 2 * 9)) {
+      if (this.game.pitches === 0) {
         this.startgame();
       }
       this.game.pitches++;
+      this.pitchResult = this.message('pitch');
 
-      var zone;
+      let zone: boolean;
 
       // Inside strike zone? [Zone% - average 45%]
       this.pitchOutcome = this.rand(1, 100);
-      if(this.pitchOutcome <= 45){
+      if (this.pitchOutcome <= 45) {
         // Inside the zone
-        zone = 1;
+        zone = true;
 
         // Swing? [Z-Swing% - average 65%]
         this.pitchOutcome = this.rand(1, 100);
-        if(this.pitchOutcome <= 65){
+        if (this.pitchOutcome <= 65) {
           // Pitch swung on
           this.swing(zone);
-        }
-        else{
+        } else {
           // Pitch taken
           this.take(zone);
         }
-      }
-      else{
+      } else {
         // Outside the zone
-        zone = 0;
+        zone = false;
 
         // Swing? [O-Swing% - average 30%]
         this.pitchOutcome = this.rand(1, 100);
-        if(this.pitchOutcome <= 30){
+        if (this.pitchOutcome <= 30) {
           // Pitch swung on
           this.swing(zone); // Contact? [O-Contact% - average 66%]
-        }
-        else{
+        } else {
           // Pitch taken
           this.take(zone);
         }
@@ -267,34 +352,70 @@ export class AppComponent {
 
       this.pitchOutput = this.pitchResult;
 
-      if (this.pa.balls == 4) {
-        this.resetpa();
-        this.pitchResult += '<strong>Ball four! That\'s a walk.</strong>';
-        this.game.BB++;
-      }
-      if (this.pa.strikes == 3) {
-        this.resetpa();
-        this.pitchResult += '<strong>Steee-rike three! Struck out.</strong>';
-        this.game.K++;
-        this.game.outs++;
-      }
-      if (this.pa.inplay == true) {
-        this.resetpa();
-        this.game.inplay++;
-      }
-
       /* reset */
-      this.pitchResult = 'Here comes the pitch...<br /><br />';
+      this.pitchResult = this.message('pitch');
     }
   }
 
-  pitches(pitches){
-    for(let i=1;i<=pitches;i++){
+  pitches(pitches: number) {
+    for (let i = 1; i <= pitches; i++) {
       this.pitch();
     }
   }
 
-  ngOnInit() {
+  ngOnChanges() {
+    // Respond when Angular (re)sets data-bound input properties.
+    // The method receives a SimpleChanges object of current and previous property values.
 
+    // Called before ngOnInit() and whenever one or more data-bound input properties change.
+
+    // Implementing DoCheck and OnChanges in a class is not recommended
+  }
+
+  ngOnInit() {
+    // Initialize the directive/component after Angular first displays
+    // the data-bound properties and sets the directive/component's input properties.
+
+    // Called once, after the first ngOnChanges().
+  }
+/*
+  ngDoCheck() {
+
+    // Detect and act upon changes that Angular can't or won't detect on its own.
+
+    // Called during every change detection run, immediately after ngOnChanges() and ngOnInit().
+
+    // Implementing DoCheck and OnChanges in a class is not recommended
+  }
+*/
+  ngAfterContentInit() {
+    // Respond after Angular projects external content into the component's view / the view that a directive is in.
+
+    // Called once after the first ngDoCheck().
+  }
+
+  ngAfterContentChecked() {
+    // Respond after Angular checks the content projected into the directive/component.
+
+    // Called after the ngAfterContentInit() and every subsequent ngDoCheck().
+  }
+
+  ngAfterViewInit() {
+    // Respond after Angular initializes the component's views and child views / the view that a directive is in.
+
+    // Called once after the first ngAfterContentChecked().
+  }
+
+  ngAfterViewChecked() {
+    // Respond after Angular checks the component's views and child views / the view that a directive is in.
+
+    // Called after the ngAfterViewInit() and every subsequent ngAfterContentChecked().
+  }
+
+  ngOnDestroy() {
+    // Cleanup just before Angular destroys the directive/component.
+    // Unsubscribe Observables and detach event handlers to avoid memory leaks.
+
+    // Called just before Angular destroys the directive/component.
   }
 }
