d862f80da386fba7bc09f550a7b1bb6db00eb81e
Dan Russell <dan.russell@uconn.edu>
2020-02-27 08:36:58 -0500

    Commiting uncommitteds

--- angular-baseball/src/app/game.service.ts
+++ angular-baseball/src/app/game.service.ts
@@ -5,34 +5,34 @@ import { Injectable } from '@angular/core';
 })
 export class GameService {
 
-constructor() { }
+  constructor() { }
 
-game = {
-  pitches: 0,
-  swings: 0,
-  misses: 0,
-  taken: 0,
-  balls: 0,
-  strikes: 0,
-  BB: 0,
-  K: 0,
-  inplay: 0,
-  hits: 0,
-  outs: 0,
-  inning: 1,
-  innings: [],
-  teams: {
-    away: {
-      runs: 0,
-      hits: 0,
-      errors: 0
-    },
-    home: {
-      runs: 0,
-      hits: 0,
-      errors: 0
+  game = {
+    pitches: 0,
+    swings: 0,
+    misses: 0,
+    taken: 0,
+    balls: 0,
+    strikes: 0,
+    BB: 0,
+    K: 0,
+    inplay: 0,
+    hits: 0,
+    outs: 0,
+    inning: 1,
+    innings: [],
+    teams: {
+      away: {
+        runs: 0,
+        hits: 0,
+        errors: 0
+      },
+      home: {
+        runs: 0,
+        hits: 0,
+        errors: 0
+      }
     }
-  }
-};
+  };
 
 }
