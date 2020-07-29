f97bff303fa42d12d9bac7ee52682948c180ccca
Dan Russell <dan.russell@uconn.edu>
2020-07-29 14:38:19 -0400

    Added CT, updated tests

--- angular-baseball/e2e/src/app.e2e-spec.ts
+++ angular-baseball/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-baseball!');
+    expect(page.getTitleText()).toEqual('ANGULARBaseball');
   });
 
   afterEach(async () => {
