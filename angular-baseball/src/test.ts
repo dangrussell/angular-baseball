148af4c3d62c1befd5f9eafdcc94161a5117e028
Dan Russell <dan.g.russell@gmail.com>
2021-07-05 00:28:20 -0400

    ng update @angular-eslint/schematics @angular/cli @angular/core --force

--- angular-baseball/src/test.ts
+++ angular-baseball/src/test.ts
@@ -1,6 +1,6 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/zone-testing'; // zone-testing needs to be first import!
+import 'zone.js/testing'; // zone-testing needs to be first import!
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
