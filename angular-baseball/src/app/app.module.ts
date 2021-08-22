51079e9f8668c3161d5ca306530512c9ec403b35
Dan Russell <4387475+dangrussell@users.noreply.github.com>
2021-08-21 23:40:29 -0400

    ng add @ngrx/store-devtools@latest

--- angular-baseball/src/app/app.module.ts
+++ angular-baseball/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LineupPlayerComponent } from './components/lineup/lineup-player/lineup-player.component';
@@ -23,7 +25,8 @@ import { pitchReducer } from './reducers/pitch.reducer';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    StoreModule.forRoot({ pitch: pitchReducer })
+    StoreModule.forRoot({ pitch: pitchReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [
     httpInterceptorProviders
