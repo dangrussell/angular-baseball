b85709d6833a80059eb3802e44866c3d78952a48
Dan Russell <4387475+dangrussell@users.noreply.github.com>
2022-10-19 09:19:30 -0400

    Rand as exported function, remove ordinal and rand functions from class

--- angular-baseball/src/app/services/var.service.ts
+++ angular-baseball/src/app/services/var.service.ts
@@ -6,6 +6,12 @@ export interface Position {
   abbreviation: string;
 }
 
+export const rand = (min: number, max: number): number => {
+  const minR: number = Math.ceil(min);
+  const maxR: number = Math.floor(max);
+  return Math.floor(Math.random() * (maxR - minR + 1)) + minR;
+};
+
 export const getOrdinal = (i: number): string => {
   const j = i % 10;
   const k = i % 100;
@@ -100,26 +106,5 @@ export class VarService {
       abbreviation: 'RF',
     }
   ];
-
-  public rand(min: number, max: number): number {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  public ordinal(i: number): string {
-    const j = i % 10;
-    const k = i % 100;
-    if (j === 1 && k !== 11) {
-      return 'st';
-    }
-    if (j === 2 && k !== 12) {
-      return 'nd';
-    }
-    if (j === 3 && k !== 13) {
-      return 'rd';
-    }
-    return 'th';
-  }
 
 }
